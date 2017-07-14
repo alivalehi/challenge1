6d1ac33bca529cc2fd939d672facd7b9b425498c
Ali <ali.valehi@gmail.com>
2017-07-14 10:07:41 -0700

    extract data updated

--- extractData.py
+++ extractData.py
@@ -1,5 +1,10 @@
 import sqlite3
+import json
 import pandas as pd
 conn = sqlite3.connect('sqlite.db')
 df = pd.read_sql_query("SELECT * FROM 'exec';", conn)
-
+for values in enumerate(df.rates):
+   Dic = json.loads((values[1]))
+   print(Dic["1"])
+   print(Dic["5"])
+   
